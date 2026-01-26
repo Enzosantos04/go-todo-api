26d5e9b301e07495fe6e8e3ed54e7612522d4bc2
enzosdev <enzohfs@gmail.com>
2026-01-25 21:15:30 -0300

    fix: add lastInsertId to return the user id when the request returns 201

--- internal/repositories/user_repo_sql.go
+++ internal/repositories/user_repo_sql.go
@@ -15,9 +15,21 @@ func NewSqliteUserRepository(db *sql.DB) *SqliteUserRepository{
 }
 
 
-func (r SqliteUserRepository) CreateUser(user *models.User) error{
-	query := "INSERT INTO users (name) VALUES(?) "
-	_, err := r.DB.Exec(query, user.Name)
-	return err
+func (r SqliteUserRepository) CreateUser(user *models.User) error {
+	query := "INSERT INTO users (name) VALUES (?)"
+
+	result, err := r.DB.Exec(query, user.Name)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	user.ID = int(id)
+	return nil
 }
+
 
