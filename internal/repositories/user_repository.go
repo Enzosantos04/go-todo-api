13bb5053d4ba736ee0508f89aacfa6e545172240
enzosdev <enzohfs@gmail.com>
2026-01-26 20:45:11 -0300

    feat: add GetUserById on interface

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -6,4 +6,5 @@ import "gin-todo-listAPI/internal/models"
 type User interface{
 	GetAllUser() ([]models.User, error)
 	CreateUser( user *models.User) error
+    GetUserById(string) (*models.User, error)
 }
