963fc883eef6baf545c1f3caf406585ce947d6cf
enzosdev <enzohfs@gmail.com>
2026-01-29 20:08:38 -0300

    chore: updated db

--- internal/repositories/task_repository.go
+++ internal/repositories/task_repository.go
@@ -4,8 +4,8 @@ import "gin-todo-listAPI/internal/models"
 
 type Task interface {
 GetAllTasks() ([]models.Task, error)
-GetTaskByID(id string) (*models.Task, error)
+// GetTaskByID(id string) (*models.Task, error)
 CreateTask(task *models.Task) error
-UpdateTaskById(id string, task *models.Task)  error 
-DeleteTaskById(id string) error;
+// UpdateTaskById(id string, task *models.Task)  error 
+// DeleteTaskById(id string) error;
 }
