dc993c9a0ca704eff68cc078f9ee9d3e9f62c766
enzosdev <enzohfs@gmail.com>
2026-01-29 19:46:57 -0300

    fix: fix some errors and typos

--- internal/handlers/task_controller.go
+++ internal/handlers/task_controller.go
@@ -1,16 +1,37 @@
 package handlers
 
 import (
+	"gin-todo-listAPI/internal/models"
 	"gin-todo-listAPI/internal/services"
+
+	"github.com/gin-gonic/gin"
 )
 
 
 
-type taskHandler struct{
+type TaskHandler struct{
 	service *services.TaskService
 }
 
-func NewTaskHandler(service *services.TaskService) *taskHandler{
-	return &taskHandler{service:  service}
+func NewTaskHandler(service *services.TaskService) *TaskHandler{
+	return &TaskHandler{service:  service}
+}
+
+
+func (h *TaskHandler) CreateTask(c *gin.Context){
+	var task models.Task
+
+	if err := c.ShouldBindJSON(&task); err != nil{
+			c.JSON(400, gin.H{"erorr": "Invalid body"})
+		return
+	}
+
+	err := h.service.CreateTask(&task)
+	if err != nil{
+	c.JSON(400, gin.H{"error": err.Error()})
+	return
+	}
+
+	c.JSON(200, task)
 }
 
