d5662e56677ec3651af618ab56a9c51b9f404f0c
enzosdev <enzohfs@gmail.com>
2026-01-29 19:05:54 -0300

    fix: change Interface name to Task and fix getTaskById and updateTaskById

--- internal/repositories/task_repository.go
+++ internal/repositories/task_repository.go
@@ -2,10 +2,9 @@ package repositories
 
 import "gin-todo-listAPI/internal/models"
 
-type TaskRepository interface {
+type Task interface {
 GetAllTasks() ([]models.Task, error)
-GetTaskByID(id int) (*models.Task, error)
+GetTaskByID(id string) (*models.Task, error)
 CreateTask(task *models.Task) error
-UpdateTask(task *models.Task) error
-DeleteTask(id int) error
+UpdateTaskById(id string, task *models.Task)  error 
 }
